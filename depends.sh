d535d46e6e4ff01fcd0b314bb2bd017925fe2ab5
Bror Gustav <bgw@brorgustav.com>
2025-06-06 12:19:55 +0200

    5

--- depends.sh
+++ depends.sh
@@ -1,7 +1,8 @@
-#/bin/bash
 sudo apt update -y
+sudo apt full-upgrade
 sudo apt install python3-pip -y
 sudo apt-get install portaudio19-dev -y
+sudo apt install pulseaudio-utils
 sudo usermod -a -G video bgw
 source ./bin/activate
 pip3 install sounddevice numpy
